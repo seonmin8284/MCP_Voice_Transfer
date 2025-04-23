840d5a3e50df74f4036b1b246303aa9ba7f6f2ed
seonmin8284 <seonmin8284@naver.com>
2025-04-23 18:36:44 +0900

    modified:   .DS_Store 	renamed:    llms/nlp_base.ipynb -> experiments/llms/nlp_base.ipynb 	renamed:    llms/readme.md -> experiments/llms/readme.md 	renamed:    llms/results.json -> experiments/llms/results.json 	renamed:    llms/results1.json -> experiments/llms/results1.json 	renamed:    llms/sampling.json -> experiments/llms/sampling.json 	modified:   frontend/README.md 	modified:   frontend/lib/data/datasources/stt/download_model.dart 	modified:   frontend/lib/data/datasources/stt/stt_service_whisper.dart 	modified:   frontend/lib/presentation/providers/stt_provider.dart 	modified:   frontend/pubspec.yaml

--- frontend/lib/data/datasources/stt/download_model.dart
+++ frontend/lib/data/datasources/stt/download_model.dart
@@ -18,22 +18,46 @@ enum WhisperModel {
   /// tiny model for all languages
   tiny("tiny"),
   tinyQ5_1("tiny-q5_1"),
+  tinyQ8_0("tiny-q8_0"),
+  tinyEn("tiny.en"),
+  tinyEnQ5_1("tiny.en-q5_1"),
+  tinyEnQ8_0("tiny.en-q8_0"),
 
   /// base model for all languages
   base("base"),
-  baseQ8_0("base-q8_0"),
   baseQ5_1("base-q5_1"),
-  smallQ8_0("small-q8_0"),
+  baseQ8_0("base-q8_0"),
+  baseEn("base.en"),
+  baseEnQ5_1("base.en-q5_1"),
+  baseEnQ8_0("base.en-q8_0"),
 
   /// small model for all languages
   small("small"),
+  smallQ5_1("small-q5_1"),
+  smallQ8_0("small-q8_0"),
+  smallEn("small.en"),
+  smallEnQ5_1("small.en-q5_1"),
+  smallEnQ8_0("small.en-q8_0"),
+  smallEnTdrz("small.en-tdrz"),
 
   /// medium model for all languages
   medium("medium"),
+  mediumQ5_0("medium-q5_0"),
+  mediumQ8_0("medium-q8_0"),
+  mediumEn("medium.en"),
+  mediumEnQ5_0("medium.en-q5_0"),
+  mediumEnQ8_0("medium.en-q8_0"),
 
   /// large model for all languages
   largeV1("large-v1"),
-  largeV2("large-v2");
+  largeV2("large-v2"),
+  largeV2Q5_0("large-v2-q5_0"),
+  largeV2Q8_0("large-v2-q8_0"),
+  largeV3("large-v3"),
+  largeV3Q5_0("large-v3-q5_0"),
+  largeV3Turbo("large-v3-turbo"),
+  largeV3TurboQ5_0("large-v3-turbo-q5_0"),
+  largeV3TurboQ8_0("large-v3-turbo-q8_0");
 
   const WhisperModel(this.modelName);
 
